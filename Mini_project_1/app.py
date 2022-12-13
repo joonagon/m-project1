5278743742f78dd5bf220d1c0274b3287ed97d9b
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 21:38:43 +0900

    hh

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.title("서울시의 모기 예측하기")
-st.header("모기예보제와 일자별 날씨 데이터를 통한, 모기 발생 빈도 변화 파악")
+st.header("서울시의 모기 발생 빈도 변화 파악")
 
 
 st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
