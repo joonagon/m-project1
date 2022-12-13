5c9c53fac75e5e0933f958121f8f3e5a6f249618
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 22:26:44 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -24,7 +24,7 @@ st.line_chart(data=mosq_data, x='모기지수 발생일',y='모기지수(공원)
 
 
 weather_data = pd.read_csv('./Mini_project_1/weather.csv', encoding='cp949')
-tempr=weather_data[:84]
+tempr = weather_data
 tempr[['일시', '평균기온(℃)']]
 st.line_chart(data=tempr, x='일시', y='평균기온(℃)')
 
