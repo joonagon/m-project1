88b2e0d9aa2f8a1840217ea36f80ac9192d7d459
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 17:21:01 +0900

    22

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# 서울시의 모기 파악하기
+## 모기예보제와 일자별 날씨 데이터를 통한 모기 발생 빈도 변화 파악
+
 st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
 st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
