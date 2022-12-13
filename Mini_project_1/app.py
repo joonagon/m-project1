ee5e064d84a17846fcc005fda87d028c572e05a1
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 17:19:31 +0900

    11

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
 st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
 # UTF-8 / CP-949
