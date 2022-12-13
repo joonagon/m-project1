853eb62cec81c699558b4a537b14e61fa659b59f
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 14:50:48 +0900

    11

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -31,7 +31,27 @@ for idx, col in enumerate(columns2):
 
 # 13명이 소속 될 조 이름을 넣을 위치
 #st.write(st.session_state)
-ss = pd.Series(st.session_state)
+#ss = pd.Series(st.session_state)
+#ss2 = ss[ss.ne("")]
+#st.write(ss2)
+
+#n_idx = ss2.index.str.contains('n')
+#n_data = ss2[n_idx]
+#st.write(n_data)
+
+#g_idx = ss2.index.str.contains('g')
+#g_data = ss2[g_idx]
+#st.write(g_data)
+
+#n_rd = np.random.choice(n_data, len(n_data), replace=False)
+#st.write(n_rd)
+#g_rd = np.random.choice(g_data, len(g_data), replace=False)
+#st.write(g_rd)
+
+# 추첨 버튼
+
+if st.button('추첨 시작!'):
+    ss = pd.Series(st.session_state)
 ss2 = ss[ss.ne("")]
 st.write(ss2)
 
@@ -43,8 +63,10 @@ g_idx = ss2.index.str.contains('g')
 g_data = ss2[g_idx]
 st.write(g_data)
 
-# 추첨 버튼
-
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
+st.write(n_rd)
+g_rd = np.random.choice(g_data, len(g_data), replace=False)
+st.write(g_rd)
 
 # 13개의 짝을 지어서 표시해줄 그래픽
-
+df = pd.DataFrame({"추췀 대상자 이름" : n_rd, "조 이름":g_rd})
