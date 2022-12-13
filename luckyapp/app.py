276b847d1b991d259b5bd420ac856278232c6ec9
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 14:20:54 +0900

    1

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -11,10 +11,15 @@ st.header('여러분의 참여를 환영합니다.')
 
 # 추첨 대상인 13명의 이름을 넣을 위치
 # 3x4 형태로 배치
+
+tabs = st.tabs(['참가자'])
+
 columns = st.columns(4)
 for idx, col in enumerate(columns):
     for idx2 in range(4) :
         col.text_input(f"조 추첨 대상 {idx+1 + idx2 *4}", key=f"n{idx+1 + idx2 *4}")
+
+
 
 # 13명이 소속 될 조 이름을 넣을 위치
 st.write(st.session_state)
