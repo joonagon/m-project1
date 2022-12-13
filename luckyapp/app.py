16c153acc65c6ce89952afd2b563d993eed87a8f
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 14:23:27 +0900

    1

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -19,7 +19,7 @@ for idx, col in enumerate(columns):
     for idx2 in range(4) :
         col.text_input(f"조 추첨 대상 {idx+1 + idx2 *4}", key=f"n{idx+1 + idx2 *4}")
 
-
+columns2=tabs[1].columns(4)
 
 # 13명이 소속 될 조 이름을 넣을 위치
 st.write(st.session_state)
