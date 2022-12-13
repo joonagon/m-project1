8daa802c703982993d200eb9cda506ae85fdec1c
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 14:28:32 +0900

    11

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -17,9 +17,12 @@ tabs = st.tabs(['참가자', '조'])
 columns = st.columns(4)
 for idx, col in enumerate(columns):
     for idx2 in range(4) :
-        col.text_input(f"조 추첨 대상 {idx+1 + idx2 *4}", key=f"g{idx+1 + idx2 *4}")
+        col.text_input(f"조 추첨 대상 {idx+1 + idx2 *4}", key=n"n{idx+1 + idx2 *4}")
 
 columns2=tabs[1].columns(4)
+for idx, col in enumerate(columns):
+    for idx2 in range(4) :
+        col.text_input(f"조 추첨 대상 {idx+1 + idx2 *4}", key="g{idx+1 + idx2 *4}")
 
 # 13명이 소속 될 조 이름을 넣을 위치
 st.write(st.session_state)
