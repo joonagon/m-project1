61df773e7d27aa64f272e25d82f120efd824b8bd
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 15:57:08 +0900

    11

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -8,5 +8,5 @@ st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
 # UTF-8 / CP-949
 # https://seong6496.tistory.com/269
-df = pd.read_csv('./opendata/mosdata.csv', encoding='cp949')
+df = pd.read_csv('./Mini_project_1/mosdata.csv', encoding='cp949')
 st.write(df)
