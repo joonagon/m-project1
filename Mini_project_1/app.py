83bd1e87077382d0186b5137c7f3396b3fcbbc55
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 17:30:19 +0900

    22

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -8,7 +8,7 @@ import seaborn as sns
 ## 모기예보제와 일자별 날씨 데이터를 통한 모기 발생 빈도 변화 파악
 
 st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
-st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
+st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#", fontcolor = '#CECECE')
 
 # UTF-8 / CP-949
 # https://seong6496.tistory.com/269
