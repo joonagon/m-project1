c55b82d8646104a1b0f75e4b133795389c02ca8b
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 12:25:01 +0900

    추첨앱 폴더생성

---
+++ getcha/Readme.txt
@@ -0,0 +1,9 @@
+# 추첨 앱 만들어보기
+* numpy.random.choice
+* 버튼을 누르면 추첨
+* 이름을 입력하는 input(list)
+* 조를 입력하는 input(list)
+---
+> streamlit 작동에 필요한 2가지
+1. ***.py : 작동시켜줄 메인 코드
+1. requirtments.txt : pip를 해야할 라이브러리를 적어놓기
---
+++ getcha/app.py
@@ -0,0 +1,18 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 웹 앱 스터디 및 테스트 용 페이지
+    ### 열심히 배우는 중...
+    * 1$ = 1300원
+    """
+)
+
+# https://unsplash.com
+# https://pixabay.com/ko
+st.image(
+    "https://cdn.pixabay.com/photo/2022/01/13/07/06/house-6934544_960_720.jpg"
+    )
+
