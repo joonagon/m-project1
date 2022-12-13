1b39b932230731979e679fe5777b716d8b308a43
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 15:26:56 +0900

    11

--- Graph_test/app.py
+++ Graph_test/app.py
@@ -5,19 +5,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.write(
-    
-    # 탐색적 데이터 분석(Exploratory Data Analysis)과 그래프
-    > EDA
-    * 그래프, 통계 수치등을 활용해서 데이터를 파악하는 과정
-    * 탐색적 데이터 분석 단계에서는 다양한 그래프가 나옴
-    * 그래프는 데이터를 한눈에 파악하는데 도움을 줌(경향성 파악)
-    1. 데이터의 구성
-    1. 어떤 변수(피처)가 중요한가
-    1. 어떻게 새로운 변수를 만들까
-    * 모델링에 필요한 다양한 정보를 얻을 수 있다.
-    * **모델링** : 변수들을 통해 답을 이끌어 내는 과정. 통계, 머신러닝, 딥러닝 등
-        1. 어떤 변수를 넣을지
-        1. 어떻게 변형 시켜야 하는가(결측치, 이상치 등)
-        1. 전처리 -> 피처 엔지니어링
-    
+    '''
+    # 탐색적 데이터 분석 (EDA) & Seaborn을 통한 데이터 시각화
+    ## 데이터의 종류
+    * 데이터의 특성에 따라서 시각화 방식도 달라짐
+    * 정형 데이터 : 값으로 나타낼 수 있는 데이터 (숫자)
+    * 비정형 데이터 : 정형 데이터가 아닌 것 (사진, 언어...)
+    ## 정형 데이터
+    |대분류|소분류|예시|
+    |:-|:-|:-|
+    |수치형 데이터(사칙 연산이 가능한 데이터)|연속형 데이터|키, 몸무게, 수입
+    ||이산형 데이터|과일 개수, 책의 페이지 수
+    |범주형 데이터(범주로 나누어지는 데이터)|순서형 데이터|학점, 순위(랭킹)|
+    ||명목형 데이터|성별, 음식종류, 우편번호|
+    # 수치형 데이터 numerical data
+    > 사칙 연산이 가능한 데이터 (+, -, *, /)
+    '''
 )
+
+# df
+titanic = sns.load_dataset('titanic')
+st.write(titanic) # 적당히 짤라줌
+# st.table(titanic) # 전체를 보여줌
+
+# seaborn를 통한 시각화
+fig = plt.figure(figsize=(8, 4))
+sns.histplot(data=titanic, x='age')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(8, 4))
+sns.histplot(data=titanic, x='age', hue='alive', multiple='stack')
+st.pyplot(fig)
