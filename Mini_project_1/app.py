b2d3f1e8df6c837857800e29c20c9ade0bc662eb
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 22:53:49 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -11,6 +11,7 @@ st.header("서울시의 모기 발생 빈도 변화 파악")
 st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
 st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
+pd.options.display.float_format = '{:,.2f}'.format
 
 # UTF-8 / CP-949
 # https://seong6496.tistory.com/269
@@ -26,7 +27,6 @@ st.line_chart(data=mosq_data, x='모기지수 발생일',y='모기지수(공원)
 
 weather_data = pd.read_csv('./Mini_project_1/weather.csv', encoding='cp949')
 
-weather_data[['평균기온(℃)']]=round(weather_data[['평균기온(℃)']], 2)
 weather_data
 
 #weather_data[['일시', '평균기온(℃)']]
