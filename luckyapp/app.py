2794d42f21bc6e4986b64aa322196f7b9d32e211
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 12:32:37 +0900

    타이틀

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -1,18 +1,12 @@
 # streamlit 라이브러리 호출
 import streamlit as st
+import numpy as np
 
-# 마크다운을 기반으로 한 꾸미기 기능 작동
-st.write(
-    """
-    # 웹 앱 스터디 및 테스트 용 페이지
-    ### 열심히 배우는 중...
-    * 1$ = 1300원
-    """
-)
+#st.write() 마크다운
 
-# https://unsplash.com
-# https://pixabay.com/ko
-st.image(
-    "https://cdn.pixabay.com/photo/2022/01/13/07/06/house-6934544_960_720.jpg"
-    )
+st.title('조 추첨 페이지')
 
+# 추첨 대상인 13명의 이름을 넣을 위치
+# 13명이 소속 될 조 이름을 넣을 위치
+# 추첨 버튼
+# 13개의 짝을 지어서 표시해줄 그래픽
