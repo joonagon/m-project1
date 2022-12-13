d662bbbbdb4a65c933d65c21f43e69939d1e9a22
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 12:38:33 +0900

    d

--- luckyapp/app.py
+++ luckyapp/app.py
@@ -8,7 +8,11 @@ st.title('조 추첨 페이지')
 st.header('여러분의 참여를 환영합니다.')
 
 # 추첨 대상인 13명의 이름을 넣을 위치
-st.text_input('조 추첨 대상')
+# 3x4 형태로 배치
+columns = st.columns(4)
+
+for c in columns:
+    st.text_input('조 추첨 대상')
 
 # 13명이 소속 될 조 이름을 넣을 위치
 
