bde37fa69358c3406542a8048de4d0aa7f8fd229
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-13 22:16:22 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -24,7 +24,8 @@ st.line_chart(data=mosq_data, x='모기지수 발생일',y='모기지수(공원)
 
 
 weather_data = pd.read_csv('./Mini_project_1/weather.csv', encoding='cp949')
-weather_data
+tempr=weather_data[:84]
+tempr
 
 
 #lp_2 = sns.lineplot(data=df2, x='모기지수 발생일',y='모기지수(주거지)')
