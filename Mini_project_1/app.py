421dc1ab5fb3d3e2d8e267c39893a51377506ab7
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-14 09:10:01 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -12,7 +12,7 @@ st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113
 st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
 
-
+st.subheader('모기 개체수에 비례한 "모기 지수" 데이터')
 # UTF-8 / CP-949
 # https://seong6496.tistory.com/269
 df1 = pd.read_csv('./Mini_project_1/mosdata.csv', encoding='cp949')
@@ -24,6 +24,7 @@ st.line_chart(data=mosq_data, x='모기지수 발생일',
                               y=['모기지수(수변부)','모기지수(주거지)','모기지수(공원)'])
 
 # 날씨 자료 링크(기상청) https://data.kma.go.kr/climate/RankState/selectRankStatisticsDivisionList.do
+st.subheader('2016년 5월부터의 서울시 기온변화')
 
 weather_data = pd.read_csv('./Mini_project_1/weather.csv', encoding='cp949')
 
