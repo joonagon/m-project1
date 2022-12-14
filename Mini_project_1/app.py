8d84f05e14fe97d84df00f0bee9d1656c2e72851
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-14 15:29:32 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -49,6 +49,11 @@ with tab2:
                                   x='모기지수 발생일',
                                   y=['모기지수(수변부)','모기지수(주거지)','모기지수(공원)'],
                                   )
+    st.subheader('날짜별 모기지수 보기') # 일부만 입력해도 출력되도록 고쳐야됨
+    title = st.text_input('원하는 날짜를 입력하세요!')
+    if title:
+        mosfx_data = mosq_data.loc[(mosq_data['모기지수 발생일'] == title +'T00:00:00')]
+        st.write(mosfx_data)
 
 
 with tab3:
