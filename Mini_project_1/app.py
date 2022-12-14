a4bdf467727d28fe7ad3bdc1726f81496bfa2ac6
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-14 11:09:42 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -28,7 +28,7 @@ with tab2:
     # st.line_chart(data=None, x=None, y=None, width=0, height=0, use_container_width=True
     st.area_chart(data=mosq_data, x='모기지수 발생일', 
                                   y=['모기지수(수변부)','모기지수(주거지)','모기지수(공원)'],
-                                  height = 100,
+                                  height = 540,
                                   use_container_width=True
                                   )
 
