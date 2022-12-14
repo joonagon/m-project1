2d71df5df9c517126ccb6bf7dbfc2349cd7bc42b
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-14 10:29:45 +0900

    123

--- Mini_project_1/app.py
+++ Mini_project_1/app.py
@@ -12,22 +12,24 @@ st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113
 st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
 
-st.subheader('모기 개체수에 비례한 "모기 지수" 데이터')
+st.subheader('모기 개체수에 비례한 모기지수 그래프')
 # UTF-8 / CP-949
 # https://seong6496.tistory.com/269
 df1 = pd.read_csv('./Mini_project_1/mosdata.csv', encoding='cp949')
 mosq_data = df1[:2517]
 
 st.markdown("")
-see_data = st.expander('데이터 시트 상세보기')
+see_data = st.expander('서울시 모기지수 데이터')
 with see_data:
     st.dataframe(data=mosq_data)
-
-
 
 # st.line_chart(data=None, x=None, y=None, width=0, height=0, use_container_width=True
 st.line_chart(data=mosq_data, x='모기지수 발생일', 
                               y=['모기지수(수변부)','모기지수(주거지)','모기지수(공원)'])
+
+
+
+
 
 # 날씨 자료 링크(기상청) https://data.kma.go.kr/climate/RankState/selectRankStatisticsDivisionList.do
 st.subheader('2016년 5월부터의 서울시 기온변화')
@@ -36,7 +38,7 @@ weather_data = pd.read_csv('./Mini_project_1/extremum_20221214093542.csv', encod
 weather_data2 = weather_data[['일시', '평균기온(℃)','최고기온(℃)','최저기온(℃)']]
 
 st.markdown("")
-see_data = st.expander('2016년부터 현재까지 서울시 기온 상세보기')
+see_data = st.expander('서울시 기온변화 상세보기(2016 ~ 2022)')
 with see_data:
     st.dataframe(data=weather_data)
 
