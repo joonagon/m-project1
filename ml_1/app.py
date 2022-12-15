5e6a1ed5c26543cbf74fb7afb73fa9f422d462ef
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-15 17:21:18 +0900

    123

--- ml_1/app.py
+++ ml_1/app.py
@@ -17,3 +17,23 @@ model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+
+# age : 나이
+st.number_input(
+    label = '나이',
+    step=1,
+    value=30,
+    key='age'
+)
+#st.session_state['age']
+#st.wirte(st.session_state['age'])
+
+
+# sex : 성별
+st.radio(
+    label='성별'
+    options=['남성', '여성'],
+    index=0
+    key='sex'
+)
+st.wirte(st.session_state['sex'])
