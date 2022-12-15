13dc36dd0d13a48bca849828fda44648f46ee6ad
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-15 17:41:20 +0900

    123

--- ml_1/app.py
+++ ml_1/app.py
@@ -13,7 +13,7 @@ st.write(df)
 import joblib
 import os
 # os.path... 파이썬 경로문제 해결
-model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
