d7436dbc7bef3d80a1025157047998ff8544640a
joonagon <119823535+joonagon@users.noreply.github.com>
2022-12-15 17:15:31 +0900

    1234

--- ml_1/app.py
+++ ml_1/app.py
@@ -4,7 +4,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+pd.options.display.float_format = '{:.2f}'.format
 st.title("머신러닝 1")
 
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+import joblib
+import os
+
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
